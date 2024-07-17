9f720ea1a4db0aa6e336daafecf0327ff47a62ee
LiuYHCode <lyh997835@163.com>
2024-07-17 22:59:05 +0800

    【修改内容】:公共代码提取，自定义模块依赖 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:公共代码提取，自定义模块依赖 【需要测试】:是

---
+++ cloud-api-commons/src/main/java/com/at2024/Main.java
@@ -0,0 +1,11 @@
+package com.at2024;
+
+/**
+ * @author lyh
+ * @date 2024-07-17 22:37:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- cloud-consumer-order80/src/main/java/com/at2024/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/at2024/cloud/controller/OrderController.java
@@ -1,7 +1,7 @@
-package com.at2024.controller;
+package com.at2024.cloud.controller;
 
-import com.at2024.entities.PayDTO;
-import com.at2024.resp.ResultData;
+import com.at2024.cloud.entities.PayDTO;
+import com.at2024.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
