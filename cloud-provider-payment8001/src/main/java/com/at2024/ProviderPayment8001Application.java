ac17a696025af5903e49467c9860f92b2c4533c8
LiuYHCode <lyh997835@163.com>
2024-07-21 12:51:27 +0800

    【修改内容】:支付模块引入consul 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:支付模块引入consul 【需要测试】:是

--- cloud-provider-payment8001/src/main/java/com/at2024/ProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/at2024/ProviderPayment8001Application.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.at2024.**.mapper")
+@EnableDiscoveryClient//开启服务发现
 public class ProviderPayment8001Application {
     private static Logger LOGGER = LoggerFactory.getLogger(ProviderPayment8001Application.class);
 
