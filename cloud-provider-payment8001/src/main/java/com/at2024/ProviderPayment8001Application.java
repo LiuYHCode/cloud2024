1f2f91a6b985b00227819cc868a226edd4190667
LiuYHCode <lyh997835@163.com>
2024-07-21 21:41:17 +0800

    【修改内容】:服务者配置consul动态刷新 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:服务者配置consul动态刷新 【需要测试】:是

--- cloud-provider-payment8001/src/main/java/com/at2024/ProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/at2024/ProviderPayment8001Application.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.at2024.**.mapper")
 @EnableDiscoveryClient//开启服务发现
+@RefreshScope//动态刷新consul服务器配置
 public class ProviderPayment8001Application {
     private static Logger LOGGER = LoggerFactory.getLogger(ProviderPayment8001Application.class);
 
