73be62792435dacb45f91d7de14ff8b971dea4d8
LiuYHCode <lyh997835@163.com>
2024-07-29 22:45:21 +0800

    【修改内容】:feign日志处理以及压缩功能 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:feign日志处理以及压缩功能 【需要测试】:是

--- cloud-consumer-feign-order80/src/main/java/com/at2024/cloud/config/FeignConfig.java
+++ cloud-consumer-feign-order80/src/main/java/com/at2024/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.at2024.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,7 @@ public class FeignConfig {
     @Bean
     public Retryer myRetryer()
     {
-        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
+//        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
 
         //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
         /**
@@ -21,6 +22,11 @@ public class FeignConfig {
          * long maxPeriod,最大的间隔时间
          * int maxAttempts 最大的重试次数
          */
-//        return new Retryer.Default(100,1,3);
+        return new Retryer.Default(100,1,3);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
