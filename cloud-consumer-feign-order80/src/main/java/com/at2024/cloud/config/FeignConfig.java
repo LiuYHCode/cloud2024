fe1a4be18c62b9e24b00f10f003a9a73a1e0f85f
LiuYHCode <lyh997835@163.com>
2024-07-29 22:13:21 +0800

    【修改内容】:Feign重试机制 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:Feign重试机制 【需要测试】:是

---
+++ cloud-consumer-feign-order80/src/main/java/com/at2024/cloud/config/FeignConfig.java
@@ -0,0 +1,26 @@
+package com.at2024.cloud.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lyh
+ * @date 2024-07-29 22:06:05
+ */
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Retryer myRetryer()
+    {
+//        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
+
+        //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
+        /**
+         * long period, 初始化时间配置，也就是100秒以后开始启动这个配置
+         * long maxPeriod,最大的间隔时间
+         * int maxAttempts 最大的重试次数
+         */
+        return new Retryer.Default(100,1,3);
+    }
+}
