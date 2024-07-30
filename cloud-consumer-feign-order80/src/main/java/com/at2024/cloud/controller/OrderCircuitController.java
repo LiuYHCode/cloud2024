a46e39c69352bcbfddaeb41e1b08724dc41a5635
LiuYHCode <lyh997835@163.com>
2024-07-30 23:22:44 +0800

    【修改内容】:bulkhead，r4j船舱、隔离效果演示 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:bulkhead，r4j船舱、隔离效果演示 【需要测试】:是

--- cloud-consumer-feign-order80/src/main/java/com/at2024/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-feign-order80/src/main/java/com/at2024/cloud/controller/OrderCircuitController.java
@@ -1,6 +1,7 @@
 package com.at2024.cloud.controller;
 
 import com.at2024.cloud.api.PayFeignApi;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,9 +23,28 @@ public class OrderCircuitController {
     {
         return payFeignApi.myCircuit(id);
     }
+
     //myCircuitFallback就是服务降级后的兜底处理方法
     public String myCircuitFallback(Integer id,Throwable t) {
         // 这里是容错处理逻辑，返回备用结果
         return "myCircuitFallback，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
     }
+
+    /**
+     *(船的)舱壁,隔离
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/feign/pay/bulkhead/{id}")
+    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadFallback",type = Bulkhead.Type.SEMAPHORE)
+    public String myBulkhead(@PathVariable("id") Integer id)
+    {
+        return payFeignApi.myBulkhead(id);
+    }
+    public String myBulkheadFallback(Throwable t)
+    {
+        return "myBulkheadFallback，隔板超出最大数量限制，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+    }
+
+
 }
