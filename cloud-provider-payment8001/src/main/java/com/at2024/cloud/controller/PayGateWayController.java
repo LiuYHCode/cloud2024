07f78715863f54233457317c623d969e49a5d2f6
LiuYHCode <lyh997835@163.com>
2024-08-04 18:49:21 +0800

    【修改内容】:gateway的过滤器添加案例及误去除部分配置回退 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:gateway的过滤器添加案例及误去除部分配置回退 【需要测试】:是

--- cloud-provider-payment8001/src/main/java/com/at2024/cloud/controller/PayGateWayController.java
+++ cloud-provider-payment8001/src/main/java/com/at2024/cloud/controller/PayGateWayController.java
@@ -1,19 +1,25 @@
 package com.at2024.cloud.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.at2024.cloud.entities.Pay;
 import com.at2024.cloud.resp.ResultData;
 import com.at2024.cloud.service.PayService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
 
 /**
  * @author lyh
  * @date 2024-08-04 11:30:30
  */
 @RestController
+@Slf4j
 public class PayGateWayController {
     @Resource
     private PayService payService;
@@ -27,6 +33,23 @@ public class PayGateWayController {
     @GetMapping(value = "/pay/gateway/info")
     public ResultData<String> getGatewayInfo() {
         return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request)
+    {
+        String result = "";
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements()) {
+            String headName = headers.nextElement();
+            String headValue = request.getHeader(headName);
+            System.out.println("请求头名: " + headName +"\t\t\t"+"请求头值: " + headValue);
+            if(headName.equalsIgnoreCase("X-Request-lyh1")
+                    || headName.equalsIgnoreCase("X-Request-lyh2")) {
+                result = result+headName + "\t " + headValue +" ";
+            }
+        }
+        return ResultData.success("getGatewayFilter 过滤器 test： "+result+ DateUtil.now());
     }
 
 }
