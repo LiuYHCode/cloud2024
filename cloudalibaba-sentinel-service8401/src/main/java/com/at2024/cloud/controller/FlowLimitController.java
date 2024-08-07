e33ef3204f1b774d5dfbbd7d60bd3df01b99ad77
LiuYHCode <lyh997835@163.com>
2024-08-07 22:44:19 +0800

    【修改内容】:流控-排队等待，熔断-慢调用比例 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:流控-排队等待，熔断-慢调用比例 【需要测试】:是

--- cloudalibaba-sentinel-service8401/src/main/java/com/at2024/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/at2024/cloud/controller/FlowLimitController.java
@@ -5,6 +5,8 @@ import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author lyh
  * @date 2024-08-07 21:14:13
@@ -37,5 +39,28 @@ public class FlowLimitController {
     public String testD() {
         flowLimitService.common();
         return "------testD";
+    }
+
+    /**
+     * 流控效果-排队等待，多出的消息丢弃
+     * @return
+     */
+    @GetMapping("/testE")
+    public String testE() {
+        System.out.println(System.currentTimeMillis()+"      testE,排队等待");
+        return "------testE";
+    }
+
+    /**
+     * 新增熔断规则-慢调用比例
+     * 10个线程，在一秒的时间内发送完。又因为服务器响应时长设置：暂停1秒，所以响应一个请求的时长都大于1秒综上符合熔断条件，所以当线程开启1秒后，进入熔断状态
+     * @return
+     */
+    @GetMapping("/testF")
+    public String testF() {
+        //暂停几秒钟线程
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println("----测试:新增熔断规则-慢调用比例 ");
+        return "------testF 新增熔断规则-慢调用比例";
     }
 }
