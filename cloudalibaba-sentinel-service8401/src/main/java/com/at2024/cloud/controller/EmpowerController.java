185875570cdb0906bcc4ae2cbbafdc5507860922
LiuYHCode <lyh997835@163.com>
2024-08-08 23:39:45 +0800

    【修改内容】:sentinel授权规则 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:sentinel授权规则 【需要测试】:是

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/at2024/cloud/controller/EmpowerController.java
@@ -0,0 +1,19 @@
+package com.at2024.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lyh
+ * @date 2024-08-08 23:33:34
+ */
+@RestController
+@Slf4j
+public class EmpowerController { //Empower授权规则，用来处理请求的来源
+    @GetMapping(value = "/empower")
+    public String requestSentinel4() {
+        log.info("测试Sentinel授权规则empower");
+        return "Sentinel授权规则";
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/at2024/cloud/handler/MyRequestOriginParser.java
@@ -0,0 +1,17 @@
+package com.at2024.cloud.handler;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lyh
+ * @date 2024-08-08 23:34:38
+ */
+@Component
+public class MyRequestOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter("serverName");
+    }
+}
