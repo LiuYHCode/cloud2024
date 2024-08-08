03499f9e3e4fe6eb94f49096b78fb7e6d9a1d60b
LiuYHCode <lyh997835@163.com>
2024-08-08 23:33:09 +0800

    【修改内容】:QPS热点限流 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:QPS热点限流 【需要测试】:是

--- cloudalibaba-sentinel-service8401/src/main/java/com/at2024/cloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/at2024/cloud/controller/RateLimitController.java
@@ -5,6 +5,7 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,6 +29,7 @@ public class RateLimitController {
         return "服务不可用@SentinelResource启动"+"\t"+"o(╥﹏╥)o";
     }
 
+    //下面该案例是异常兜底及正常访问限流
     @GetMapping("/rateLimit/doAction/{p1}")
     @SentinelResource(value = "doActionSentinelResource",
             blockHandler = "doActionBlockHandler", fallback = "doActionFallback")
@@ -37,14 +39,24 @@ public class RateLimitController {
         }
         return "doAction";
     }
-
     public String doActionBlockHandler(@PathVariable("p1") Integer p1, BlockException e){
         log.error("sentinel配置自定义限流了:{}", e);
         return "sentinel配置自定义限流了";
     }
-
     public String doActionFallback(@PathVariable("p1") Integer p1,Throwable e){
         log.error("程序逻辑异常了:{}", e);
         return "程序逻辑异常了"+"\t"+e.getMessage();
+    }
+
+    //热点限流，QPS
+    //热点可以设置参数例外项，保证特殊数值的限流规则改变
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        return "------testHotKey";
+    }
+    public String dealHandler_testHotKey(String p1,String p2,BlockException exception) {
+        return "-----dealHandler_testHotKey";
     }
 }
