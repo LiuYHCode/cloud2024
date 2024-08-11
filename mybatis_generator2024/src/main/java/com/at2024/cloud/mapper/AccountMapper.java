2708eb065c6a73b630df84d35ebf1b774f8ff240
LiuYHCode <lyh997835@163.com>
2024-08-11 15:17:55 +0800

    【修改内容】:seata对应使用mybatis一键生成 【提交类型】:阶段提交 【项管系统】:无 【详细描述】:seata对应使用mybatis一键生成 【需要测试】:是

---
+++ mybatis_generator2024/src/main/java/com/at2024/cloud/mapper/AccountMapper.java
@@ -0,0 +1,7 @@
+package com.at2024.cloud.mapper;
+
+import com.at2024.cloud.entities.Account;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AccountMapper extends Mapper<Account> {
+}
---
+++ mybatis_generator2024/src/main/java/com/at2024/cloud/mapper/StorageMapper.java
@@ -0,0 +1,7 @@
+package com.at2024.cloud.mapper;
+
+import com.at2024.cloud.entities.Storage;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface StorageMapper extends Mapper<Storage> {
+}
